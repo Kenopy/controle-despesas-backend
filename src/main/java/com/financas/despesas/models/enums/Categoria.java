25013d5e3f5333987b6dbd5194f08117616767a4
Eduardo Duarte <xavier.duarte43@gmail.com>
2026-06-07 13:43:59 -0300

    feat: criar enums Categoria e FormaPagamento

---
+++ src/main/java/com/financas/despesas/models/enums/Categoria.java
@@ -0,0 +1,12 @@
+package com.financas.despesas.models.enums;
+
+public enum Categoria {
+    ALIMENTACAO,
+    CASA,
+    LAZER,
+    SAUDE,
+    TRANSPORTE,
+    EDUCACAO,
+    GAMES,
+    OUTROS
+}
---
+++ src/main/java/com/financas/despesas/models/enums/FormaPagamento.java
@@ -0,0 +1,9 @@
+package com.financas.despesas.models.enums;
+
+public enum FormaPagamento {
+    PIX,
+    DINHEIRO,
+    CARTAO_CREDITO,
+    CARTAO_DEBITO,
+    BOLETO
+}
